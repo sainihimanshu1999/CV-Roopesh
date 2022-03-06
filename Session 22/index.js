950820035a10c3720fd6b2bbdb91903be7a04ab0
Himanshu Saini <himanshu@pop-os.localdomain>
2022-03-06 11:51:25 +0530

    session 22

---
+++ Session 22/index.js
@@ -0,0 +1,64 @@
+
+document.getElementById('first').innerHTML = 'Hello i am an javascript element cool';
+
+// let and const are the two type of variables which we can define
+// let - the values in it can be reassgined
+// const -  once the value is set, it can't be changed
+
+let a = 'Roopesh' // it is string data, '' single quotes
+let b = "plays" // it is also string data type, "" double quotes
+let c = 80 // it is numbers data type
+let d = 68217.90809 //it isnumber data type, supports decimal too, floating point
+let h // undefined datatype
+let r = null; // null datatype which means empty
+let f = 5
+let j = 6
+
+const pivot = 908 
+
+
+document.write(a+'<br>')
+document.write(b + '<br>')
+document.write(c+'<br>')
+document.write(d+'<br>')
+document.write(h+'<br>')
+document.write(r+'<br>')
+document.write(f==j) // == is a comparison operator boolean operator
+document.write('<br>')
+
+document.write(pivot + '<br>')
+
+
+
+
+let x = 67
+let y = 90
+
+document.write(x!=y + '<br>')
+
+
+let m = 90
+ // m = m -20 - m = 90 -20 = 70
+m %=2 // m = m%20 = 90/2 = 1 
+document.write(m + '<br>')
+
+
+let st1 = 'Hello'
+
+let st2 ="i'm good"
+
+document.write(st1+' '+st2 +'<br>')
+
+
+let df = 9
+
+df++ // df = df+1
+
+document.write(df)
+
+
+
+
+
+
+
