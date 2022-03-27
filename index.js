453f9b0deb27b3a994c6ac5654267d452dd4988e
Himanshu Saini <himanshu@pop-os.localdomain>
2022-03-27 11:43:36 +0530

    session 25

---
+++ index.js
@@ -0,0 +1,23 @@
+function roop(){
+    alert('hello roopesh')
+}
+
+//function functionname(parameters)
+function roopesh(a,b,c){
+    document.write(a+b+c)
+    document.write('<br>')
+
+    
+}
+
+// call the function we just made
+
+function xo(a,b){
+    document.write('Hello my name is '+ a + ' and my age is '+ b )
+    document.write('<br>')
+}
+
+xo('Himanshu','22')
+
+roopesh(3,4,5)
+roopesh(88,2390,89)
