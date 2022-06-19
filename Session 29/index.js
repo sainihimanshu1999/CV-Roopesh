2ac1ff14573a3bf3879e92027237fe472a542df0
Himanshu Saini <himanshu@pop-os.localdomain>
2022-06-19 15:41:33 +0530

    session 29

---
+++ Session 29/index.js
@@ -0,0 +1,25 @@
+document.write('Hello from java script','<br>')
+document.write('again','<br>')
+document.getElementById('roop').innerHTML = 'ROOOPPPPPEEESSSSSHHHH';
+document.getElementById('roop').style.color= 'red';
+//document.getElementsByClassName('ko').innerHTML = 'BLUE elephant'
+
+function gun(){
+    alert('The form was submitted')
+}
+
+function over(){
+    document.write('pointer is over the element')
+}
+
+function out(){
+    document.write('Pointer is out of the element')
+}
+
+function load(){
+    alert('Page is loaded')
+}
+
+function key(){
+    alert('pressed a key in input ')
+}
